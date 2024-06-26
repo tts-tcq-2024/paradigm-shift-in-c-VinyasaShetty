fbdc3fa82966c0a5b8d5272a3cf914eaa1176000
VinyasaShetty <vinyasa.u.shetty@gmail.com>
2024-06-26 11:18:19 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,7 +2,8 @@
 #include <assert.h>
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  ((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8)) ? return 0: return 1;
+  ((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8)) ?  0:  1;
+  return 0;
 }
   
 
